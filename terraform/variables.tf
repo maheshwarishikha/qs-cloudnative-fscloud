65b19d55f69764628704b34f7d9be1d2ed52f6a4
Chuck Cox <chuckc@us.ibm.com>
2021-09-03 17:23:43 -0500

    chore: updating for vpc cluster

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,21 +30,31 @@ variable "cluster_name" {
 variable "cluster_namespace" {
   type        = string
   description = "Kubernetes namespace to deploy into. NOTE: If the namespace does not exist, it will be created."
+  default     = "default"
 }
 
 variable "default_pool_size" {
-  default     = "3"
+  type        = string
   description = "Number of worker nodes for the new Kubernetes cluster"
+  default     = "3"
+}
+
+variable "datacenter" {
+  type        = string
+  description = "Zone from `ibmcloud ks zones --provider vpc-classic`"
+  default     = "us-south-1"
 }
 
 variable "machine_type" {
-  default     = "b3c.4x16"
+  type        = string
   description = "Name of machine type from `ibmcloud ks flavors --zone <ZONE>`"
+  default     = "bx2.4x16"
 }
 
 variable "kube_version" {
-  default     = "4.6.34_openshift"
+  type        = string
   description = "Version of Kubernetes to apply to the new Kubernetes cluster (Run: `ibmcloud ks versions` to see available versions)"
+  default     = "4.6.42_openshift"
 }
 
 variable "branch" {
@@ -58,14 +68,3 @@ variable "ibmcloud_api_key" {
   description = "The IAM API Key for IBM Cloud access (https://cloud.ibm.com/iam/apikeys)"
 }
 
-variable "toolchain_template_repo" {
-  type        = string
-  description = "URL for the Toolchain Template"
-  default     = "https://github.com/open-toolchain/secure-kube-toolchain"
-}
-
-variable "application_repo" {
-  type        = string
-  description = "URL for the application to deploy"
-  default     = "https://github.com/IBM/example-bank"
-}
